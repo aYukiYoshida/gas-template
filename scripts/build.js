ea6f3b5f74b38f72cbfcbdf20a5f6d76d430c0f8
aYukiYoshida <yuki.yoshida@access-company.com>
2025-01-22 17:09:44 +0900

    feat: add commander package

--- scripts/build.js
+++ scripts/build.js
@@ -1,9 +1,21 @@
 import { GasPlugin } from 'esbuild-gas-plugin';
 import { build }  from 'esbuild';
+import { Command } from "commander";
 
-build({
-  entryPoints: ['src/app.ts'],
-  bundle: true,
-  outfile: 'public/app.js',
-  plugins: [GasPlugin]
-}).catch(() => process.exit(1));
+const buildApp = () => {
+  build({
+    entryPoints: ['src/app.ts'],
+    bundle: true,
+    outfile: 'public/app.js',
+    plugins: [GasPlugin]
+  }).catch(() => process.exit(1));
+};
+
+const program = new Command();
+program
+  .description("build application")
+  .action(() => {
+      buildApp();
+});
+
+program.parse(process.argv);
