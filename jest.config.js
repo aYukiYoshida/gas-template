94f5e3d477c3ac3ac3a6462ff7b93874bcff86e1
aYukiYoshida <yuki.yoshida@access-company.com>
2025-02-12 00:25:31 +0900

    test: update configuration for jest

--- jest.config.js
+++ jest.config.js
@@ -145,7 +145,7 @@ const config = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  // testEnvironment: "jest-environment-node",
+  testEnvironment: "node",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -173,7 +173,9 @@ const config = {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  transform: {
+    "^.+.tsx?$": ["ts-jest",{"tsconfig": "tsconfig.json",}],
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
