b7b1be2bc9ad6af250e5f12c1e093e5cc12a4b8f
jyoppomu <jyoppomu@gmail.com>
2026-01-21 14:44:14 +0900

    refactor: add JSDoc to main function

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import functions from "./functions";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare let global: any;
+/**
+ * this is sample function
+ * @returns {void}
+ */
 global.main = functions.main;
